e578b7fdb3d7360ffa40573cafe1771e2ef0d083
AntonyHaro <antonyhpro@outlook.com>
2025-02-26 20:22:08 -0300

    crud com prisma + sqlite funcionando

--- src/app/api/tasks/route.js
+++ src/app/api/tasks/route.js
@@ -3,6 +3,19 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+// Obter todas as tarefas (GET)
+export async function GET() {
+    try {
+        const tasks = await prisma.task.findMany();
+        return NextResponse.json(tasks, { status: 200 });
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Erro ao buscar tarefas" },
+            { status: 500 }
+        );
+    }
+}
+
 // Criar uma nova tarefa (POST)
 export async function POST(req) {
     try {
@@ -17,23 +30,12 @@ export async function POST(req) {
     }
 }
 
-// Obter todas as tarefas (GET)
-export async function GET() {
-    try {
-        const tasks = await prisma.task.findMany();
-        return NextResponse.json(tasks, { status: 200 });
-    } catch (error) {
-        return NextResponse.json(
-            { error: "Erro ao buscar tarefas" },
-            { status: 500 }
-        );
-    }
-}
-
 // Atualizar uma tarefa (PUT)
 export async function PUT(req) {
+    // adicionar verificações de CAMPOSSSSS
     try {
         const { id, title, description, completed } = await req.json();
+        console.log({ id, title, description, completed });
         const task = await prisma.task.update({
             where: { id },
             data: { title, description, completed },
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import {
@@ -27,17 +27,12 @@ import {
 import { Trash2, Check, Pencil, Plus } from "lucide-react";
 
 export default function Home() {
-    const [tasks, setTasks] = useState([
-        {
-            id: "1",
-            name: "TASK 1",
-            description: "Codar uma aplicação web em Next.js + Prisma",
-            isComplete: false,
-        },
-    ]);
+    const [tasks, setTasks] = useState([]);
     const [successMessage, setSuccessMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
     const [isDialogOpen, setIsDialogOpen] = useState(false);
     const [editingTask, setEditingTask] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     const {
         register,
@@ -47,63 +42,153 @@ export default function Home() {
         formState: { errors },
     } = useForm();
 
+    const getTasks = async () => {
+        try {
+            const response = await fetch("/api/tasks", {
+                method: "GET",
+            });
+
+            const data = await response.json();
+
+            response.ok
+                ? setTasks(data)
+                : setErrorMessage(`Error: ${data.error}`);
+        } catch (error) {
+            setErrorMessage("Error while fetching tasks.");
+        }
+    };
+
     // Função para adicionar ou editar uma task
-    const onSubmit = (data) => {
+    const onSubmit = async (data) => {
         if (errors.name || errors.description) return;
+
+        const task = {
+            ...(data.id && { id: data.id }),
+            title: data.title,
+            description: data.description,
+        };
 
         if (editingTask) {
             // Editando uma tarefa existente
-            setTasks((prevTasks) =>
-                prevTasks.map((task) =>
-                    task.id === editingTask.id
-                        ? {
-                              ...task,
-                              name: data.name,
-                              description: data.description,
-                          }
-                        : task
-                )
-            );
-            setSuccessMessage("Task updated successfully!");
+            try {
+                const response = await fetch("/api/tasks", {
+                    method: "PUT",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(task),
+                });
+
+                const data = await response.json();
+
+                if (response.ok) {
+                    setSuccessMessage("Task updated successfully!");
+
+                    setTasks((prevTasks) =>
+                        prevTasks.map((t) => (t.id === data.id ? data : t))
+                    );
+                } else {
+                    setErrorMessage(`Error: ${data.error}`);
+                }
+            } catch (error) {
+                setErrorMessage(`Error while editing task.`);
+            }
         } else {
-            // Criando uma nova tarefa
-            const newTask = {
-                id: crypto.randomUUID(),
-                name: data.name,
-                description: data.description,
-                isComplete: false,
-            };
-            setTasks([...tasks, newTask]);
-            setSuccessMessage("Task added successfully!");
+            try {
+                const response = await fetch("/api/tasks", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(task),
+                });
+
+                const data = await response.json();
+
+                if (response.ok) {
+                    setSuccessMessage("Task added successfully!");
+                } else {
+                    setErrorMessage(`Error: ${data.error}`);
+                }
+
+                setTasks([...tasks, data]);
+            } catch (error) {
+                setErrorMessage("Error while creating task.");
+            }
         }
 
         reset();
+        setLoading(false);
         setEditingTask(null);
         setIsDialogOpen(false);
         setTimeout(() => setSuccessMessage(""), 3000);
+        setTimeout(() => setErrorMessage(""), 3000);
     };
 
     // Marcar como concluída
-    const handleTaskComplete = (taskId) => {
-        setTasks((prevTasks) =>
-            prevTasks.map((task) =>
-                task.id === taskId ? { ...task, isComplete: true } : task
-            )
-        );
+    const handleTaskComplete = async (task) => {
+        task.completed = true;
+
+        try {
+            const response = await fetch("/api/tasks", {
+                method: "PUT",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(task),
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                setSuccessMessage("Task completed successfully!");
+            } else {
+                setErrorMessage(`Error: ${data.error}`);
+            }
+
+            setTasks((prevTasks) =>
+                prevTasks.map((t) => (t.id === data.id ? data : t))
+            );
+        } catch (error) {
+            setErrorMessage("Error while completing task.");
+        }
+
+        setTimeout(() => setSuccessMessage(""), 3000);
+        setTimeout(() => setErrorMessage(""), 3000);
     };
 
     // Deletar uma tarefa
-    const handleDeleteTask = (taskId) => {
-        setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+    const handleDeleteTask = async (id) => {
+        try {
+            const response = await fetch("/api/tasks", {
+                method: "DELETE",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ id }),
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                setSuccessMessage("Task deleted successfully!");
+            } else {
+                setErrorMessage(`Error: ${data.error}`);
+            }
+
+            setTasks((prevTasks) => prevTasks.filter((t) => t.id !== id));
+        } catch (error) {
+            setErrorMessage("Error while deleting task.");
+        }
+
+        setTimeout(() => setSuccessMessage(""), 3000);
+        setTimeout(() => setErrorMessage(""), 3000);
     };
 
     // Abrir o modal para edição
     const handleEditTask = (task) => {
         setEditingTask(task);
-        setValue("name", task.name);
+        setValue("id", task.id);
+        setValue("title", task.title);
         setValue("description", task.description);
         setIsDialogOpen(true);
     };
+
+    useEffect(() => {
+        getTasks();
+    }, []);
 
     return (
         <div className="p-6 mx-auto space-y-4 flex flex-col items-center justify-center min-h-screen">
@@ -134,12 +219,12 @@ export default function Home() {
                         className="grid gap-4 py-4"
                     >
                         <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="name" className="text-right">
+                            <Label htmlFor="title" className="text-right">
                                 Name
                             </Label>
                             <Input
-                                id="name"
-                                {...register("name", {
+                                id="title"
+                                {...register("title", {
                                     required: "Task name is required",
                                 })}
                                 className="col-span-3"
@@ -193,6 +278,12 @@ export default function Home() {
                 </div>
             )}
 
+            {errorMessage && (
+                <div className="bg-red-100 text-red-700 p-2 rounded">
+                    {errorMessage}
+                </div>
+            )}
+
             {/* Lista de tarefas */}
             <div className="border rounded-lg p-4 w-full max-w-4xl break-words whitespace-normal">
                 <Table>
@@ -209,13 +300,13 @@ export default function Home() {
                         {tasks.map((task) => (
                             <TableRow
                                 key={task.id}
-                                className={
-                                    task.isComplete
+                                className={`hover:bg-gray-200 ${
+                                    task.completed
                                         ? "bg-green-100 text-green-700"
                                         : ""
-                                }
+                                }`}
                             >
-                                <TableCell>{task.name}</TableCell>
+                                <TableCell>{task.title}</TableCell>
                                 <TableCell>{task.description}</TableCell>
                                 <TableCell className="flex gap-1 flex-wrap justify-center">
                                     <Button
@@ -234,10 +325,8 @@ export default function Home() {
                                     </Button>
                                     <Button
                                         className="p-3"
-                                        onClick={() =>
-                                            handleTaskComplete(task.id)
-                                        }
-                                        disabled={task.isComplete}
+                                        onClick={() => handleTaskComplete(task)}
+                                        disabled={task.completed}
                                     >
                                         <Check />
                                     </Button>
